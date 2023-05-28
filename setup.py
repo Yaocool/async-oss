765591eb93e03ffda4afee15ab5e1e54630a8d17
ozzy <ozzycharon@gmail.com>
2023-05-29 02:36:38 +0800

    chore: update requirements.txt and lib info

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ from setuptools import setup, find_packages
 setup(
     name='async-oss',
     version='1.0.0',
-    description='An asynchronous OSS library based on OSS2=2.15.0.',
-    author='longyao',
-    author_email='longyao@91jkys.com',
+    description='An asynchronous OSS library.',
+    author='Ozzy',
+    author_email='ozzycharon@gmail.com',
     license='MIT',
     install_requires=['aiohttp', 'oss2'],
     packages=find_packages(),
