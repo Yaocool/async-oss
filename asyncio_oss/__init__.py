06bdd4cdbab1ea66ede264d836e2a7389d6909e3
ozzy <ozzycharon@gmail.com>
2023-06-27 01:54:11 +0800

    feat: support global log

--- asyncio_oss/__init__.py
+++ asyncio_oss/__init__.py
@@ -1,12 +1,16 @@
+from oss2 import set_file_logger as oss_set_file_logger, set_stream_logger as oss_set_stream_logger
 from oss2.auth import Auth
 
-from asyncio_oss.api import Service, Bucket
-from asyncio_oss.iterators import (
+from .api import Service, Bucket
+from .iterators import (
     BucketIterator,
     ObjectIterator,
+    ObjectIteratorV2,
     MultipartUploadIterator,
     ObjectUploadIterator,
     PartIterator, LiveChannelIterator)
+
+import logging
 
 __all__ = [
     'Auth', 'Service', 'Bucket', 'BucketIterator',
@@ -16,3 +20,11 @@ __all__ = [
     'PartIterator',
     'LiveChannelIterator'
 ]
+
+
+def set_file_logger(file_path, name='asyncio_oss', level=logging.INFO, format_string=None):
+    oss_set_file_logger(file_path, name, level, format_string)
+
+
+def set_stream_logger(name='asyncio_oss', level=logging.DEBUG, format_string=None):
+    oss_set_stream_logger(name, level, format_string)
