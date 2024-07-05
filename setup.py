0c36024ae75af0412a46c53374013041466a9058
ozzy <ozzycharon@gmail.com>
2024-07-06 00:43:26 +0800

    chore: bump version to 1.1.5

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name='asyncio-oss',
-    version='1.1.4',
+    version='1.1.5',
     description='An asynchronous python client SDK for OSS(Aliyun Object Storage Service).',
     long_description=long_description,
     long_description_content_type="text/markdown",
